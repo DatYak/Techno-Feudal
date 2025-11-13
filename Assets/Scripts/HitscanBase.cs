d769a6112dd6301fb3cf3960668df16712e7f61f
Isaac Lovy <isaaclovy@gmail.com>
2025-11-13 16:01:02 -0500

    Hitscan "projectile" option

---
+++ Assets/Scripts/HitscanBase.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class HitscanBase : MonoBehaviour
+{
+
+    [SerializeField]
+    protected float distance;
+
+    [SerializeField]
+    protected int damage;
+
+    [SerializeField]
+    LineRenderer lineRenderer;
+
+    [SerializeField]
+    float lineDuration;
+
+    float spawnTime;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RaycastHit Hit;
+        lineRenderer.SetPosition(0, Vector3.zero);
+
+        if (Physics.Raycast(transform.position, transform.forward, out Hit, distance))
+        {
+            EnemyHealth HP;
+            if (HP = Hit.collider.gameObject.GetComponent<EnemyHealth>())
+            {
+                HP.Damage(damage);
+            }
+            lineRenderer.SetPosition(1, transform.InverseTransformPoint(Hit.point));
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, transform.InverseTransformPoint(transform.position + transform.forward * distance));
+        }
+
+        spawnTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time >  spawnTime + lineDuration) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
